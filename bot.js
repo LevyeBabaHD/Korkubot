ed468f543f4d26b6161afa47a82289c23431bc24
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-29 21:01:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,4 +154,5 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(client.login(process.env.TOKEN);
+
